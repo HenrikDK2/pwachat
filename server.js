0ba6c47c832aa4439270d402388c2589946d8f03
HenrikDK2 <henrik265@outlook.dk>
2020-09-04 09:59:20 +0200

    Update

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const expressStaticGzip = require("express-static-gzip");
 const app = express();
 const http = require("http").Server(app);
